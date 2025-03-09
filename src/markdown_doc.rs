9bf5cbb9b4a45d61711e84c0e76c23ce9ce8a83b
Sam Rowe <sgrowe@live.co.uk>
2025-03-09 14:51:10 +0000

    Fix front matter parsing

--- src/markdown_doc.rs
+++ src/markdown_doc.rs
@@ -9,28 +9,34 @@ impl<'a> MarkdownDoc<'a> {
     pub fn parse(markdown: &'a str) -> Self {
         // `markdown` will be a markdown document with a frontmatter section enclosed between two lines of three dashes
 
-        let lines = markdown.lines();
+        let mut lines = markdown.lines();
 
-        let mut front_matter_start = None;
-        let mut front_matter_end = None;
+        // Extract front matter
+        let mut fm_start = None;
+        let mut fm_end = None;
 
-        for line in lines {
-            if line == "---" {
-                let offset = line.as_ptr() as usize - markdown.as_ptr() as usize;
+        while let Some(line) = lines.next() {
+            if line != "---" {
+                continue;
+            }
 
-                if front_matter_start.is_none() {
-                    front_matter_start = Some(offset);
-                } else {
-                    front_matter_end = Some(offset);
-                    break;
+            if fm_start.is_none() {
+                if let Some(line) = lines.next() {
+                    let offset = (line.as_ptr() as usize) - markdown.as_ptr() as usize;
+
+                    fm_start = Some(offset);
                 }
+            } else {
+                let offset = (line.as_ptr() as usize) - markdown.as_ptr() as usize;
+                fm_end = Some(offset);
+                break;
             }
         }
 
-        match front_matter_start.zip(front_matter_end) {
+        match fm_start.zip(fm_end) {
             Some((s, e)) => Self {
-                frontmatter: &markdown[s..e],
-                body: &markdown[e..],
+                frontmatter: &markdown[s..e].trim_end(),
+                body: &markdown[e..].trim_end(),
             },
             None => {
                 // No front matter found
@@ -54,8 +60,7 @@ mod tests {
             fs::read_to_string("src/fixtures/01_args.md").expect("Failed to read fixture file");
         let doc = MarkdownDoc::parse(&markdown);
 
-        assert!(doc.frontmatter.contains("args:"));
-        assert!(doc.frontmatter.contains("- FUNCTION"));
-        assert!(doc.body.contains("# Add unit tests for FUNCTION"));
+        dbg!(doc.frontmatter);
+        assert!(doc.frontmatter == ("args:\n  - FUNCTION"));
     }
 }
